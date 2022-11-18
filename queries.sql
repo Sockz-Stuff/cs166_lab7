dc8a16d61a3f2f47a4c89ff7963e981a36449577
Marshall Jones <mjone032@wch135-01.cs.ucr.edu>
2022-11-18 15:35:30 -0800

    fin

--- queries.sql
+++ queries.sql
@@ -36,26 +36,38 @@ group by supplier
 where s.supplier_id  = x.supplier and x.supplier = y.supplier and x.total > y.total 
 order by s.supplier_id
 ;
-/* Q4 */
+/* Q4 *//*
 select distinct p.supplier
 from part_nyc p
 where p.part_number not in (
 select part_number
 from part_sfo)
 order by p.supplier
+;*/
+
+select distinct s.supplier_id, s.supplier_name
+from supplier s, part_nyc p
+where s.supplier_id=p.supplier and p.part_number in
+(select p.part_number
+from supplier s, part_nyc p
+where s.supplier_id=p.supplier
+except
+select q.part_number
+from supplier s, part_sfo q
+where s.supplier_id=q.supplier)
+order by s.supplier_id;
+
+/*
+select distinct s.supplier_name
+from supplier s,part_nyc p, part_sfo q
+where s.supplier_id=p.supplier and p.part_number not in(q.part_number)
 ;
-
-
+*/
 
 /* Q5 */
 update part_nyc
 set on_hand = on_hand - 10;
 
-update part_sfo
-set on_hand = on_hand - 10;
-
-
 /* Q6 */
 delete from part_nyc where on_hand < 30;
-delete from part_sfo where on_hand < 30;
 
